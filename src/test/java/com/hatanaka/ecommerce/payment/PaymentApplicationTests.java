ee1819be2cf918296af0c68fc3e2f719ccf26bd6
Daniel Hatanaka <dhatanaka@uolinc.com>
2020-06-25 15:45:04 -0300

    first commit

---
+++ src/test/java/com/hatanaka/ecommerce/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatanaka.ecommerce.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
